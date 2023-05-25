ae5b21181279dd8b4532caaacc17313715d05b32
Samson Paul <samson.paul@hotmail.com>
2023-05-24 23:41:56 -0400

    sourced an seeded data to test, updated index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,9 @@
 const { prompt } = require("inquirer");
-const logo = require("asciiart-logo");
 const db = require("./db");
 
 init();
 
-// Display logo text, load main prompts
 function init() {
-  const logoText = logo({ name: "Employee Manager" }).render();
-
-  console.log(logoText);
-
   loadMainPrompts();
 }
 
